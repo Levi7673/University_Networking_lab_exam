973f0c61e83c30452efd90fc85aa55c6f437359f
Anandhakrishnan NK <kunjumon9844@gmail.com>
2026-05-17 03:38:46 +0000

    snw

--- SNW/client.c
+++ SNW/client.c
@@ -1,41 +1,83 @@
+//c (timeval)
 #include<stdio.h>
 #include<unistd.h>
-#include<sys/time.h>
 #include<arpa/inet.h>
 #include<string.h>
+#include<time.h>
+#include<stdlib.h>
 
 #define PORT 8080
 #define SIZE 1024
 #define TIMEOUT_SECOND 3
 
 int main(){
-    struct timeval receivedTimeout;
-    char packetSendTo[SIZE];
-    char receviedMessage[SIZE];
     int clientSocket;
+    struct timeval receivedTimeout;
     struct sockaddr_in serverAddress;
+    packetToSend[SIZE];
+    packetReceived[SIZE];
 
-    srand(time(0));
+    //socket creatiom
     clientSocket = socket(AF_INET,SOCK_STREAM,0);
-    if(clientSocket < 0){
-        perror("Socket failed.");
-        exit(1);
-    }
 
+    //configure the server 
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(PORT);
     serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    connect(clientSocket,
-        (struct sockaddr*)&serverAddress,
-        sizeof(serverAddress));
-    
-    receivedTimeout.tv_sec = TIMEOUT_SECOND;
-    receivedTimeout.tv_usec = 0;
-    setsockopt(clientSocket,
-            SOL_,
-            SO_RCVTIMEO,
-            );
+    //configure timeval
+    receivedTimeout.tc_sec = TIMEOUT_SECOND;
+    receivedTimeout.usec = 0;
 
+    //set socket option 
+    setsockopt(clientSocket,
+            SOL_SOCKET,
+            SO_RCVTIMEO,
+            sizeof(receivedTimeout),
+            &receivedTimeout);
+    
+    if(connect(clientSocket,
+            (struct sockaddr*)&serverAddress,
+            sizeof(serverAddress))<0){
+                perror("connection failed");
+                exit(1);//exit with failure
+            }
+    
+    while(1){
+        printf("Enter packet number to send /'exit' : ");
+        scanf("%d",packetToSend);
+
+        if(strcmp(packetTosend,"exit")==0){
+            send(clientSocket,
+                packetToSend,
+                strlen(packetToSend),
+                0);
+            printf("[EXIT] : conection closing.\n");
+            break;
+        }
+
+        int ackReceived=0;
+        while(!ackReceived){
+            send(clientSocket,
+                packetToSend,
+                strlen(packetToSend),
+                0);
+            
+            int bytedRecevied = read(clientSocket,
+                                    packetReceived,
+                                    sizeof(packetReceived));
+            if(bytedRecevied < 0){
+                printf("[TIMEOUT] : ack not received.\n")
+                printf("[RETRANSMIT] : retransmitting the packet %d.\n",packetToSend);
+                continue;
+            }
+            packetReceived[bytedRecevied]='\0';
+            if(strcmp(packetReceived,"ACK")==0){
+                printf("[ACK] : ack received.\n");
+                ackReceived = 1;
+            }
+        }
+    }
+    close(clientSocket);
     return 0;
 }
--- SNW/server.c
+++ SNW/server.c
@@ -1,42 +1,70 @@
-#include<string.h>
-#include<arpa/inet.h>
+//probablility srand rand()%100
 #include<stdio.h>
-#include<stdlib.h>
+#include<unistd.h>
+#include<arpa/inet.h>
+#include<string.h>
 #include<time.h>
+#include<stdlib.h>
 
-#define ACK_PROB 70
 #define PORT 8080
+#define SIZE 1024
+#define ACK_PROB 70
 
 int main(){
-    int clientSocket , serverAddress;
-    struct sockaddr_in serverAddress,clientAddress;
+    int clientSocket,serverSocket;
+    struct sockaddr_in clientAddress , serverAddress;
     socklen_t clientLength = sizeof(clientAddress);
-    
-    //socket creation 
-    clientSocket = socket(AF_INET,SOCK_STREAM,0);
+    char buffer[SIZE];
 
-    //confgure the server 
+     //socket creatiom
+    serverSocket = socket(AF_INET,SOCK_STREAM,0);
+
+    //configure the server 
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(PORT);
     serverAddress.sin_addr.s_addr = INADDR_ANY;
 
-    //bind
-    if(bind(clientSocket,
-        (struct sockaddr*)&serverAddress)<0){
-            perror("bind failed");
-            exit(1);//exit with failure 
-        }
-
+    if(bind(serverSocket,
+            (struct sockaddr*)&serverAddress,
+            sizeof(serverAddress))<0){
+                perror("bind failed.");
+                exit(1);
+            }
     if(listen(serverSocket,5)<0){
-        perror("listen failed");
-        exit(1);//exit with filure 
+                perror("listen failed.");
+                exit(1);
+            }
+    
+    clientSocket = accept(serverSocket,
+                        (struct sockaddr*)&clientAddress,
+                        &clientLength);
+    srand(time(0));
+    while(1){
+        int bytedRecevied = read(clientSocket,
+                                buffer,
+                                sizeof(buffer));
+        if (bytedRecevied < 0){
+            break;
+        }
+        
+        buffer[bytedRecevied]='\0';
+
+        if(strcmp(buffer,"exit")==0){
+            printf("[EXIT] : conection closing.\n");
+            break;
+        }
+        printf("[PACKET RECECIVED] : received packet is %s",buffer)
+        if(rand()%100 < ACK_PROB ){
+            send(clientSocket,
+                "ACK",
+                strlen("ACK"),
+                0);
+            printf("[ACK] : ack send.\n");
+        }else{
+            printf("[ACK] : ack lost.\n");
+        }
     }
-
-    clientSocket = accept(serverSocket, 
-                    (struct sockaddr*)&clientAddress,
-                    &clientLength);
-    
-    
-
+    close(clientSocket);
+    close(serverSocket);
     return 0;
 }
